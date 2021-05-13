ac2d4ae6cc76705412d397a7e682eee411bb696b
like1emon <2441489234@qq.com>
2021-05-14 01:20:46 +0800

    更新

---
+++ src/config.js
@@ -0,0 +1,88 @@
+export default {
+    // page1部分
+    page1: {
+        titleEn: '你好呀，我是林锐耀',
+        title: '一个在前端路上攀爬的人', //中文标题
+    },
+    // page2部分
+    page2: {
+        authorImg: 'page1.png', // 作者头像
+        xinhui: '我叫林锐耀，是一名大三的学生，就读于吉林大学珠海学院：软件工程专业。', // 幸会
+        qiuzhi: '前端开发实习生/产品助理实习生', // 求职意向
+        guanyuwo: '由于对创造的热爱，我选择了编程。一开始是选择java后端开发，之后发现自己更适合于前端开发，便扎头进前端的海洋。现在的我掌握了扎实html与css的基础，对于vuejs与webpack都有了一定的理解。我对产品也有着强烈的热爱，大学期间我参加了许多的创新创业大赛，对产品有许多的理解。' // 关于我
+    },
+    // page3部分
+    page3: [{
+        icon: 'icon-diannao', // 图标
+        title: '图形处理', // 标题
+        msg: ['从PSD上切出页面需要的图片'] //介绍
+    }, {
+        icon: 'icon-shouji',
+        title: '网页制作',
+        msg: ["css动画效果", "响应式页面(vue)"]
+    }, {
+        icon: 'icon-tubiao-',
+        title: '前端功能',
+        msg: ['JS完成常见的前端功能', '用Axios读取后台数据']
+    }, {
+        icon: 'icon-qianbi1',
+        title: '软件文档的撰写',
+        msg: ['软件uml建模', '软件需求规格说明书的撰写']
+    }],
+    // page4部分
+    page4: {
+
+        course: [{
+            date: '2020/9——止今', // 时间
+            desc: { // 经历
+                title: '珠海市职业技能———图形图像应用技术员证书',
+                list: ['']
+            }
+        }, {
+            date: '2019/9——2020/9',
+            desc: {
+                title: '国家励志奖学金',
+                list: ['优秀班干部']
+            }
+        }, {
+            date: '2018/9——2019/9',
+            desc: {
+                title: '中国大学生创新创业互联网+广东省分赛区铜奖',
+                list: ['一等奖学金', '国家励志奖学金', '吉林大学珠海学院优秀学生']
+            }
+        }],
+
+        singlelist: [{
+            title: 'bootsrap',
+            text: '了解'
+        }, {
+            title: 'photoshop',
+            text: '掌握'
+        }, {
+            title: 'offices 办公',
+            text: '掌握'
+        }, {
+            title: 'UML 建模',
+            text: '掌握'
+        }],
+        // 我的基本技能掌握
+        proresslist: [{
+                title: 'HTML',
+                value: '85%'
+            }, {
+                title: 'CSS',
+                value: '80%'
+            }, {
+                title: 'Vue',
+                value: '75%'
+            },
+            {
+                title: 'JS',
+                value: '90%'
+            }
+        ]
+    },
+    // page5部分
+    page5: []
+
+}
